bbaa119a9387a035d5ae8ae6873bc12cd5aaf3b5
HodaHelmy4002 <hoh4002@qatar-med.cornell.edu>
2025-09-30 11:48:59 +0300

    Update Gemma2B_text_generation.py
    cleaned the code for finetuning

--- Gemma2B_text_generation.py
+++ Gemma2B_text_generation.py
@@ -9,100 +9,43 @@ import time
 import datetime
 import gc
 import random
-# from nltk.corpus import stopwords
 import re
 import transformers
 import torch.nn as nn
 from sklearn.model_selection import train_test_split
-# from sklearn.metrics import classification_report
-# from sklearn.metrics import classification_report, accuracy_score, roc_auc_score, f1_score
-# from sklearn.preprocessing import label_binarize
 import torch.nn.functional as F
 import evaluate 
 from datasets import (
-    load_dataset,
     Dataset,
     DatasetDict)
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     BitsAndBytesConfig,
-    TrainingArguments,
-    pipeline,
-    logging,
 )
 from peft import (
     LoraConfig,
     prepare_model_for_kbit_training,
-    get_peft_model
+    get_peft_model, 
+    PeftModel
 )
 from torch.utils.data import (
-    TensorDataset,
-    DataLoader,
-    RandomSampler,
-    SequentialSampler,
-    random_split,
     Dataset)
 
-#from trl import SFTTrainer
-from peft import PeftModel
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
 os.environ['TORCH_USE_CUDA_DSA'] = "1"
 os.environ["CUDA_LAUNCH_BLOCKING"] = '1'
 os.environ['TRANSFORMERS_OFFLINE'] = '0'
 
-w_token ="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"
-r_token ="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"
+w_token ='YOUR_TOKEN'
+r_token ='YOUR_TOKEN'
 
+##Data Loading and pre-processing from CSV file 
 
-# In[2]:
+############################################################
 
-
-# from huggingface_hub import login
-
-
-
-commit_message = "Fine-tuning google/gemma-2b-it on ICU dataset with shorter prompt"
-
-model_name = "google/gemma-2b-it"
-
-
-
-# In[4]:
-
-
-#commit_message = "Fine-tuning llama2-chat-7b on ICU dataset with shorter prompt"
-bnb_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_use_double_quant=True,
-    bnb_4bit_quant_type="nf4",
-    bnb_4bit_compute_dtype=torch.bfloat16
-)
-
-#"TheBloke/Mistral-7B-Instruct-v0.2-GPTQ"
-model = AutoModelForCausalLM.from_pretrained(model_name, #model_name
-                                             quantization_config=bnb_config,
-                                             device_map="auto",# automatically figures out how to best use CPU + GPU for loading model
-                                             token=r_token,
-                                             trust_remote_code=False) # prevents running custom model files on your machine
-                                             #revision="main") # which version of model to use in repo
-
-tokenizer = AutoTokenizer.from_pretrained(model_name, #model_name
-                                        token=r_token, add_eos_token=True, use_fast=True)
-
-
-# In[5]:
-
-
-tokenizer.padding_side = 'right'
-tokenizer.pad_token = tokenizer.eos_token
-tokenizer.add_eos_token = True
-tokenizer.add_bos_token, tokenizer.add_eos_token
-
-
-
-# In[6]:
-
+#Loading the files (Full dataset)
 
 def load_file(data_file):
     df = pd.read_csv(data_file)
@@ -113,10 +56,7 @@ def load_file(data_file):
 file_path_2 = '/acfs-home/hoh4002/serag_AI_lab/users/hoh4002/eICU/llm.csv'
 df2 = load_file(file_path_2)
 
-
-# In[7]:
-
-
+#Data Pre_processing
 def filter_unwanted_words(diseases, unwanted_words):
     return [disease for disease in diseases if not any(word in disease for word in unwanted_words)]
 
@@ -125,7 +65,7 @@ def create_prompts(df):
     prompts = []
     admission_responses = []
     
-    unwanted_words = ['performed']  # Add any other unwanted words here
+    unwanted_words = ['performed'] 
     
     disease_columns = [col for col in df.columns if col.startswith('Diagnose_')]
     
@@ -168,36 +108,63 @@ def create_prompts(df):
         ICU Stay Duration: {row['unitdischargeoffset']} minutes
 
         """
-        # Admission Type: {admission_type}
-        # Response: {admission_response}
 
-        # Add the comment to the list
         prompts.append(comment.strip())
         admission_responses.append(admission_response.strip())
 
     return prompts, admission_responses
 
 
-# In[8]:
-
-
+#Creating the prompts and answers 
 prompts, answers = create_prompts(df2)
 
 
-# In[9]:
 
 
-print(prompts[300])
-print(answers[300])
+#print(prompts[300])
+#print(answers[300])
+
+#print(prompts[14])
+#print( answers[14])
+
+#######################################################################
 
 
-# In[9]:
+# commit message and model name
+commit_message = "Fine-tuning google/gemma-2b-it on ICU dataset with shorter prompt"
+
+model_name = "google/gemma-2b-it"
 
 
-print(prompts[14])
-print( answers[14])
+#Configurations 
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
+
+#model and tokenizer loading 
+model = AutoModelForCausalLM.from_pretrained(model_name, #model_name
+                                             quantization_config=bnb_config,
+                                             device_map="auto",# automatically figures out how to best use CPU + GPU for loading model
+                                             token=r_token,
+                                             trust_remote_code=False) # prevents running custom model files on your machine
+                                             #revision="main") # which version of model to use in repo
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, #model_name
+                                        token=r_token, add_eos_token=True, use_fast=True)
+
+
+tokenizer.padding_side = 'right'
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.add_eos_token = True
+tokenizer.add_bos_token, tokenizer.add_eos_token
 
 model.eval() # model in evaluation mode (dropout modules are deactivated)
+
+
+#Instruction strings for the prompt
 instructions_string = """
 You are a virtual assistant in the intensive care unit (ICU), specialized in analyzing patient data and answering questions.
 
@@ -213,7 +180,7 @@ output_string = """
 input = prompts[14]
 
 prompt =f'''<start_of_turn>user\n {instructions_string} {input} \n Would this patient get re-admitted to Intensive Care Unit?<end_of_turn>\n {output_string} \n <start_of_turn>model\n '''
-# generate output
+# generate output (Example to see how the model perform)
 inputs = tokenizer(prompt, return_tensors="pt")
 outputs = model.generate(input_ids=inputs["input_ids"].to("cuda"), max_new_tokens=1024, temperature=0.1, do_sample = True)
 #print("without instruction")
@@ -221,25 +188,23 @@ print('------------------------------------------------')
 print(tokenizer.batch_decode(outputs)[0])
 
 
-# In[10]:
+
+# instructions_string = """
+# You are a virtual assistant in the intensive care unit (ICU), specialized in analyzing patient data and answering questions.
+
+#     """
+
+# output_string = """
+#    **Response:**
+#     - Analyse the patient information
+#     - Answer with "Yes" or "No" for likelihood of re-admission.
+#     - Give me label One admission or Multiple Admission
+#     - Explain your answer, detailing why re-admission is likely or unlikely based on the provided information.
+# """
 
 
-instructions_string = """
-You are a virtual assistant in the intensive care unit (ICU), specialized in analyzing patient data and answering questions.
 
-    """
-
-output_string = """
-   **Response:**
-    - Analyse the patient information
-    - Answer with "Yes" or "No" for likelihood of re-admission.
-    - Give me label One admission or Multiple Admission
-    - Explain your answer, detailing why re-admission is likely or unlikely based on the provided information.
-"""
-
-# In[12]:
-
-
+#Spliting the dataset
 train_summaries, temp_summaries, train_labels, temp_labels = train_test_split(
     prompts, answers, test_size=0.1, shuffle=True, random_state=0, stratify=answers
 )
@@ -255,6 +220,7 @@ dev = pd.DataFrame({'summary': dev_summaries, 'label': dev_labels})
 # Create a DataFrame for the test set with separate columns for summaries and labels
 test = pd.DataFrame({'summary': test_summaries, 'label': test_labels})
 
+#Saving the dataset splits for productivity
 train.to_csv("train_set_r32.csv")
 dev.to_csv("dev_set_r32.csv")
 test.to_csv("test_set_r32.csv")
@@ -271,6 +237,7 @@ dev_list = dev.to_dict(orient='records')
 test_list = test.to_dict(orient='records')
 
 from datasets import Dataset, DatasetDict
+
 dataset = DatasetDict({
     'train': Dataset.from_list(train_list),
     'dev': Dataset.from_list(dev_list),
@@ -278,8 +245,7 @@ dataset = DatasetDict({
 })
 
 
-# In[14]:
-
+#Fine tuning the model! 
 
 model.train() # model in training mode (dropout modules are activated)
 
@@ -313,10 +279,7 @@ model = get_peft_model(model, config)
 model.print_trainable_parameters()
 
 
-# In[15]:
 
-
-from datasets import Dataset, DatasetDict
 train_list = train.to_dict(orient='records')
 dev_list = dev.to_dict(orient='records')
 dataset = DatasetDict({
@@ -325,9 +288,7 @@ dataset = DatasetDict({
 })
 dataset
 
-
-# In[16]:
-
+#Create an example template
 example_template = lambda text, label: {
     'example': f'''<start_of_turn>user \n {instructions_string} \n{text} Based on his admission diagnoses, will this patient get readmitted to Intensive Care Unit?\n<end_of_turn>\n {output_string} \n <start_of_turn>model\n ''' + "\n" + label 
 }
@@ -368,19 +329,13 @@ tokenized_data = dataset.map(tokenize_function, batched=True)
 tokenizer.pad_token = tokenizer.eos_token
 
 
-# In[17]:
-
-
 # setting pad token
 tokenizer.pad_token = tokenizer.eos_token
 # data collator
 data_collator = transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False)
 
 
-# In[22]:
-
-
-# hyperparameters
+# hyperparameters for finetuning
 lr = 1e-4
 batch_size = 16
 num_epochs = 4
@@ -405,10 +360,6 @@ training_args = transformers.TrainingArguments(
     optim="paged_adamw_8bit",
 )
 
-
-# In[21]:
-
-
 # configure trainer
 trainer = transformers.Trainer(
     model=model,
@@ -419,24 +370,18 @@ trainer = transformers.Trainer(
 )
 print("----------------------- START TRAINING -----------------------")
 
-# train model
-model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+
+model.config.use_cache = False 
 trainer.train()
 
+#Saving the model locally
 trainer.save_model("/acfs-home/hoh4002/serag_AI_lab/users/hoh4002/eICU/Gemma_August/best_output_r32")
-
-
-# In[35]:
-
 
 fine_tuned_model ="gemma_r32_unbalanced"
 trainer.model.save_pretrained(fine_tuned_model)
 print("saved")
 
-
-# In[36]:
-
-
+#Merging the model with Lora weights 
 base_model = AutoModelForCausalLM.from_pretrained(
     model_name,
     low_cpu_mem_usage = True,
@@ -445,15 +390,8 @@ base_model = AutoModelForCausalLM.from_pretrained(
     device_map = {"": 0}
 )
 
-
-# In[37]:
-
-
 fine_tuned_merged_model = PeftModel.from_pretrained(base_model, fine_tuned_model)
 fine_tuned_merged_model = fine_tuned_merged_model.merge_and_unload()
-
-
-# In[38]:
 
 
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code = True)
@@ -462,9 +400,7 @@ tokenizer.save_pretrained("gemma_r32_unbalanced")
 tokenizer.padding_side = "right"
 
 
-# In[39]:
-
-
+#Pushing the model to hugging face 
 tokenizer.pad_token = tokenizer.eos_token
 fine_tuned_merged_model.push_to_hub(fine_tuned_model, use_temp_dir=False,token=w_token)
 trainer.model.push_to_hub(fine_tuned_model, use_temp_dir=False,token=w_token)
